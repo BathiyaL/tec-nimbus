2f14ae46e88295dda517ff67ba4b334a4b595984
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2025-07-25 23:55:29 +1200

    init api-testing-playwright project

---
+++ api-testing-playwright/tests/api.test.ts
@@ -0,0 +1,12 @@
+import { test, expect, request } from '@playwright/test';
+
+test('GET post by ID', async ({ request }) => {
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  expect(response.status()).toBe(200);
+  const data = await response.json();
+
+  expect(data).toHaveProperty('id', 1);
+  expect(data.title).toBeDefined();
+  console.log("TEST PASSED with status code : " + response.status());
+});
