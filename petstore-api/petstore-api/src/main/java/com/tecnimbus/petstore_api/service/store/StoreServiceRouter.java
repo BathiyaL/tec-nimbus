27be133977e6e875c8a4ea4057cfe3da90ed9211
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2025-10-18 23:19:55 +1300

    add postman demo tests

--- petstore-api/petstore-api/src/main/java/com/tecnimbus/petstore_api/service/store/StoreServiceRouter.java
+++ petstore-api/petstore-api/src/main/java/com/tecnimbus/petstore_api/service/store/StoreServiceRouter.java
@@ -3,7 +3,9 @@ package com.tecnimbus.petstore_api.service.store;
 import com.tecnimbus.petstore_api.model.external.ExternalInventoryDTO;
 import com.tecnimbus.petstore_api.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StoreServiceRouter extends BaseService {
     @Autowired
     RemoteStoreService remoteStoreService;
