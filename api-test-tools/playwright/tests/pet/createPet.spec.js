38e8e022341c5f760775c74f17e8b136e30bf831
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2026-03-23 23:58:12 +1300

    add fixtures

--- api-test-tools/playwright/tests/pet/createPet.spec.js
+++ api-test-tools/playwright/tests/pet/createPet.spec.js
@@ -5,6 +5,11 @@ import { createPetPayload } from '../../utils/dataGenerator.js';
 test('@smoke Create a pet', async ({ request }) => {
   const petApi = new PetAPI(request);
   const payload = createPetPayload();
+  // console.log('Request URL:', 'https://petstore.swagger.io/v2/pet');
+  console.log('Request Payload:', JSON.stringify(payload, null, 2));
   const response = await petApi.createPet(payload);
+  console.log('Request URL:', response.url());
+
+  console.log('Create Pet Response:', response.json());
   expect(response.status()).toBe(200);
 });
